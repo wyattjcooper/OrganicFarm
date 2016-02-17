f5402b4a7fd5998569d48b38b57b09d2ff599563
Wyatt Cooper <wcooper17@cmc.edu>
2016-02-16 20:29:51 -0800

    Adding CropManager Activity

---
+++ OrganicFarm/app/src/main/java/com/kaylaflaten/organicfarm/CropManager.java
@@ -0,0 +1,13 @@
+package com.kaylaflaten.organicfarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CropManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_manager);
+    }
+}
