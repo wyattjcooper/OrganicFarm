75f47b662e77e3abf7bfc3ed19c96528e162e2b3
Kayla Flaten <kflaten17@cmc.edu>
2016-02-17 10:40:58 -0800

    Sections/Beds
    Added xml files for sections and beds

---
+++ OrganicFarm/app/src/main/java/com/kaylaflaten/organicfarm/beds.java
@@ -0,0 +1,39 @@
+package com.kaylaflaten.organicfarm;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Kayla Flaten on 2/16/2016.
+ */
+public class beds  extends AppCompatActivity {
+
+    ListView beditems;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        beditems = (ListView) findViewById(R.id.bedItems);
+        /*Check which section was clicked; change next line with xml name */
+
+        beditems.setAdapter(new ArrayAdapter<String>(beds.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.sectionList)));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
